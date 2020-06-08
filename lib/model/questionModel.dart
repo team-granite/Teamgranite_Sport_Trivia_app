f7d076bc1f6577801637c5a650965f61e5bf59cd
Molusoj <molusoj@gmail.com>
2020-06-09 00:41:59 +0100

    commit

--- lib/model/questionModel.dart
+++ lib/model/questionModel.dart
@@ -1,34 +1,27 @@
 class QuestionModel{
-  String question;
-  String answer;
-  String imageurl;
+  final Results results;
+  
 
-  QuestionModel({this.question, this.answer, this.imageurl});
+  QuestionModel({this.results});
+  factory QuestionModel.fromJson(Map<String, dynamic> parsedJson){
+    return QuestionModel(
+      results: Results.fromJson(parsedJson['results']),
+    );
+  }
+}
 
-  void setQuestion(String getQuestion){
-    question = getQuestion;
+class Results{
+  final String question;
+  final String correct_answer;
+
+  Results({this.question, this.correct_answer});
+
+  factory Results.fromJson(Map<String, dynamic> parsedJson){
+      return Results(
+      question: parsedJson['question'],
+      correct_answer: parsedJson['correct_answer'],
+      );
   }
 
-    void setAnswer(String getAnswer){
-    answer = getAnswer;
-  }
-
-    void setImageUrl(String getImageUrl){
-    imageurl = getImageUrl;
-  }
-
-  String getQuestion(){
-    return question;
-  }
-
-  String getAnswer(){
-    return answer;
-  }
-
-  String getImageUrl(){
-    return imageurl;
-  }
-
-
-
+  
 }
