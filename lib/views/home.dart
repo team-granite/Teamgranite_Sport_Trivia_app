d0c4c245f4bd48f818f57cdb2e5753ca6e418361
chidiarua <chidiarua99@gmail.com>
2020-06-09 21:50:25 +0100

    splash and home screens

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -7,20 +7,27 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold( 
+      child: Scaffold(
+        backgroundColor: Color(0xff071a35),
+
         body: Center(
           child: InkWell(
 
             onTap: (){
               Navigator.pushReplacement(context, MaterialPageRoute(builder:(context) =>  QuizPlay()  ));
             },
-                      child: Container(
+            child: Container(
               padding: EdgeInsets.symmetric(vertical:12, horizontal:54),
               decoration: BoxDecoration(
-                color: Colors.blue,
+                color: Colors.red,
                 borderRadius: BorderRadius.circular(24),
               ),
-              child: Text("Start Quiz", style: TextStyle(color: Colors.white, fontSize: 18),),
+              child: Text("Start Quiz",
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold
+                ),),
             ),
           ),
         ),
